b45b8e8f8131144ce20991122c4d8845baef80d5
Oleg Pyshenkin <olegpyshenkin@Olegs-MacBook-Pro.local>
2024-02-10 10:03:55 -0800

    first commit

---
+++ automation.py
@@ -0,0 +1,22 @@
+# from selenium import webdriver
+# from selenium.webdriver.firefox.service import Service
+
+# # service_obg = Service()
+# # driver = webdriver.Safari(service=service_obg) #for safari
+
+# service_obg = Service()
+# driver = webdriver.Firefox(service=service_obg)
+
+
+# driver.maximize_window()
+# driver.get("https://rahulshettyacademy.com")
+# print(driver.title)
+# print(driver.current_url)
+# driver.get("https://google.com")
+# driver.back()
+# driver.refresh()
+# driver.forward()
+# driver.minimize_window()
+
+
+################
